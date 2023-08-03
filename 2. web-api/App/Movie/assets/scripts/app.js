e545c0110f137e6f6e6100d05df9b6ffb3f291b5
SeungHanLee <swq3064@naver.com>
2023-08-03 10:12:35 +0900

    js/web/chap02 실전예제2: 영화 추가모달 닫기기능 -backdorp,취소 닫기 이벤트 추가

--- 2. web-api/App/Movie/assets/scripts/app.js
+++ 2. web-api/App/Movie/assets/scripts/app.js
@@ -22,12 +22,31 @@ const CLASS_VISIBLE = 'visible';
 
 
 // =====  이벤트 핸들러 및 이벤트 바인딩 ===== //
-
 // 영화 추가 모달창을 띄우는 핸들러
 const showMovieModalHandler = e => {
   $addMovieModal.classList.add(CLASS_VISIBLE);
   $backdrop.classList.add(CLASS_VISIBLE);
 };
+const closeAddModal = () => {
+    $backdrop.classList.remove(CLASS_VISIBLE);
+    $addMovieModal.classList.remove(CLASS_VISIBLE);
+}
+
+
+// 백드롭 영역을 클릭하면 모달이 닫히는 핸들러
+const backdropHandler = e => {
+    closeAddModal();
+}
+// 영화 추가 모달창의 취소버튼을 누르면 모달이 닫히는 핸들러
+const closeMovieModalHandler = e => {
+    closeAddModal();
+};
 
 // Add movie버튼 클릭이벤트
 $addMovieButton.addEventListener('click', showMovieModalHandler);
+
+// backdrop 영역 클릭이벤트
+$backdrop.addEventListener('click', backdropHandler);
+
+// Add movie모달 취소버튼 클릭이벤트
+$cancelAddMovieButton.addEventListener('click', closeMovieModalHandler);
